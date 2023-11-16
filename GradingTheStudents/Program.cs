3fabf44a956c088a790f9a444604536bcb516828
Administrator <Administrator@DESKTOP-95RB6IG>
2023-11-16 18:14:24 +0100

    Codice Ottimizzato 2

--- GradingTheStudents/Program.cs
+++ GradingTheStudents/Program.cs
@@ -4,31 +4,32 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        List<Studente> studenti = new List<Studente>();
-
-        studenti.Add(new Studente("Marco", 10));
-        studenti.Add(new Studente("Alessio", 3));
-        studenti.Add(new Studente("Alessia", 7));
-        studenti.Add(new Studente("Marta", 6));
-        studenti.Add(new Studente("Stefano", 4));
-        studenti.Add(new Studente("Francesca", 5));
-        studenti.Add(new Studente("Mirco", 9));
-        studenti.Add(new Studente("Emanuela", 8));
-        studenti.Add(new Studente("Paolo", 6));
-        studenti.Add(new Studente("Pamela", 2));
+        List<Studente> studenti = new List<Studente>()
+        {
+            new Studente("Marco", 10),
+            new Studente("Alessio", 3),
+            new Studente("Alessia", 7),
+            new Studente("Marta", 6),
+            new Studente("Stefano", 4),
+            new Studente("Francesca", 5),
+            new Studente("Mirco", 9),
+            new Studente("Emanuela", 8),
+            new Studente("Paolo", 6),
+            new Studente("Pamela", 2)
+    };
 
 
         //---------------------------------------------------------------------------------------------
 
         double media = studenti.Average(studente => studente.voto);
 
-        Console.WriteLine($"Media Voti: {media}");
+        Console.WriteLine($"Media Voti: {media}\n");
 
         //---------------------------------------------------------------------------------------------
 
         var studenteConVotoPiùAlto = studenti.OrderBy(studente => studente.voto).Last();
 
-        Console.WriteLine($"Lo studente con il voto più alto è {studenteConVotoPiùAlto} ");
+        Console.WriteLine($"Lo studente con il voto più alto è {studenteConVotoPiùAlto}\n");
 
 
 
